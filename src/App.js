4c232627ca5d5224d09f79c597e3457e240e4379
phantom05 <monster2jy@gmail.com>
2019-02-07 23:54:27 +0900

    React : Lift cycle study

--- src/App.js
+++ src/App.js
@@ -12,9 +12,23 @@ import './App.css';
 // import VaildationSample from './VaildationSample';
 // import MyComponent from './MyComponent';
 // import ScrollBox from './ScrollBox';
-import IterationSample from './IterationSample';
+// import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+
+function getRandomColor(){
+  return '#'+Math.floor(Math.random()* 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color:getRandomColor()
+    })
+  }
   render(){
     return(
       <Fragment>
@@ -23,7 +37,9 @@ class App extends Component {
       {/* <VaildationSample /> */}
       {/* <ScrollBox ref={(ref)=>this.scrollBox = ref} />
       <button onClick={()=> this.scrollBox.scrollToBottom()}>맨 밑으로</button> */}
-      <IterationSample />
+      {/* <IterationSample /> */}
+      <button onClick={this.handleClick}>랜덤 색상</button>
+      <LifeCycleSample color={this.state.color}/>
       </Fragment>
     )
   }
